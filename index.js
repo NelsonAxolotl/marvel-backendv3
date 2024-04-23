647898dc8a0ec54964ff0c2e9e6ebef61bb3a4e8
NELSON PARAISO <nelsonparaiso@mbp-de-nelson.home>
2024-04-23 11:03:12 +0200

    id

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const axios = require('axios');
-const cors = require('cors');
+const axios = require("axios");
+const cors = require("cors");
 app.use(cors());
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
@@ -14,181 +14,274 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI);
 const User = require("./models/user");
 
-const BASE_URL = 'https://lereacteur-marvel-api.herokuapp.com';
+const BASE_URL = "https://lereacteur-marvel-api.herokuapp.com";
 const validApiKey = process.env.VALID_API_KEY;
 // Clé API valide //
 
-
 //EXTRA/////
 // Récupérer les informations d'un personnage spécifique
-app.get('/character/:characterId', async (req, res) => {
-    const { characterId } = req.params;
-    try {
-        const response = await axios.get(`${BASE_URL}/character/${characterId}`, {
-            params: { apiKey: validApiKey },
-        });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des informations du personnage' });
-    }
+app.get("/character/:characterId", async (req, res) => {
+  const { characterId } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/character/${characterId}`, {
+      params: { apiKey: validApiKey },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la récupération des informations du personnage",
+    });
+  }
 });
 
 //liste des personnages//
-app.get('/characters', async (req, res) => {
-    const apiKey = req.query.apiKey; // la clé API de ma requête
+app.get("/characters", async (req, res) => {
+  const apiKey = req.query.apiKey; // la clé API de ma requête
 
-    console.log('Clé API reçue:', apiKey);
+  console.log("Clé API reçue:", apiKey);
 
-    try {
-        const name = req.query.name || "";
-        const skip = req.query.skip || "0";
-        const limit = req.query.limit || "100";
+  try {
+    const name = req.query.name || "";
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
 
-        const response = await axios.get(`${BASE_URL}/characters`, {
-            params: { apiKey: validApiKey, name, skip, limit },
-        });
-        console.log(response.data);
-        res.json(response.data);
-
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des personnages' });
-    }
+    const response = await axios.get(`${BASE_URL}/characters`, {
+      params: { apiKey: validApiKey, name, skip, limit },
+    });
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des personnages" });
+  }
 });
-
 
 // Récupérer la liste des comics
 
-app.get('/comics', async (req, res) => {
-    const apiKey = req.query.apiKey; // la clé API de ma requête
-    try {
+app.get("/comics", async (req, res) => {
+  const apiKey = req.query.apiKey; // la clé API de ma requête
+  try {
+    const skip = req.query.skip || "0";
+    const limit = req.query.limit || "100";
+    const title = req.query.title || "";
 
-
-        const skip = req.query.skip || "0";
-        const limit = req.query.limit || "100";
-        const title = req.query.title || "";
-
-        const response = await axios.get(`${BASE_URL}/comics`, {
-            params: { apiKey: validApiKey, title, skip, limit },
-        });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des comics' });
-    }
+    const response = await axios.get(`${BASE_URL}/comics`, {
+      params: { apiKey: validApiKey, title, skip, limit },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des comics" });
+  }
 });
 
 // Récupérer les comics contenant un personnage spécifique
 
-app.get('/comics/:characterId', async (req, res) => {
-    const { characterId } = req.params;
-    try {
-
-        const response = await axios.get(`${BASE_URL}/comics/${characterId}`, {
-            params: { apiKey: validApiKey },
-        });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des comics contenant ce personnage' });
-    }
+app.get("/comics/:characterId", async (req, res) => {
+  const { characterId } = req.params;
+  try {
+    const response = await axios.get(`${BASE_URL}/comics/${characterId}`, {
+      params: { apiKey: validApiKey },
+    });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "Erreur lors de la récupération des comics contenant ce personnage",
+    });
+  }
 });
 
-
 // Récupérer les informations d'un comic spécifique
-app.get('/comic/:comicId', async (req, res) => {
-    const { comicId } = req.params;
-    try {
-        const response = await axios.get(`${BASE_URL}/comic/${comicId}?apiKey=${process.env.API_KEY}`
+app.get("/comic/:comicId", async (req, res) => {
+  const { comicId } = req.params;
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/comic/${comicId}?apiKey=${process.env.API_KEY}`
+    );
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la récupération des informations du comic",
+    });
+  }
+});
 
-        );
-        console.log(response.data);
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des informations du comic' });
+app.get("/favorites/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    // Trouver l'utilisateur par son ID
+    const user = await User.findById(userId).populate("favorites");
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
     }
+
+    // Récupérer les favoris de l'utilisateur
+    const favorites = user.favorites;
+
+    return res.status(200).json({ favorites });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des favoris" });
+  }
 });
 
 /*------SING UP-----*/
 
 app.post("/signup", async (req, res) => {
-    try {
-        console.log(req.body);
-        const existingUser = await User.findOne({ email: req.body.email });
-        if (existingUser) {
-            return res.status(409).json({ message: "This email already exists" });
-        }
-
-        if (!req.body.username || !req.body.email || !req.body.password) {
-            return res.status(400).json({ message: "Username, email, and password are required." });
-        }
-
-        const salt = uid2(16);
-        const hash = SHA256(req.body.password + salt).toString(encBase64);
-        const token = uid2(64);
-        const newUser = new User({
-            email: req.body.email,
-            account: {
-                username: req.body.username,
-            },
-            token: token,
-            salt: salt,
-            hash: hash,
-
-        });
-
-        console.log(newUser);
-        await newUser.save();
-        const responseObject = {
-            _id: newUser._id,
-            token: newUser.token,
-            account: {
-                username: newUser.account.username,
-            },
-        };
-        return res.status(201).json(responseObject);
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    console.log(req.body);
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(409).json({ message: "This email already exists" });
     }
+
+    if (!req.body.username || !req.body.email || !req.body.password) {
+      return res
+        .status(400)
+        .json({ message: "Username, email, and password are required." });
+    }
+
+    const salt = uid2(16);
+    const hash = SHA256(req.body.password + salt).toString(encBase64);
+    const token = uid2(64);
+    const newUser = new User({
+      email: req.body.email,
+      account: {
+        username: req.body.username,
+      },
+      token: token,
+      salt: salt,
+      hash: hash,
+    });
+
+    console.log(newUser);
+    await newUser.save();
+    const responseObject = {
+      _id: newUser._id,
+      token: newUser.token,
+      account: {
+        username: newUser.account.username,
+      },
+    };
+    return res.status(201).json(responseObject);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 /*--------LOGIN----*/
 
-
 app.post("/login", async (req, res) => {
-    try {
-        const loginUser = await User.findOne({ email: req.body.email });
-        if (!loginUser) {
-            return res.status(401).json({ message: "Email or password incorrect" });
-        }
-        if (!req.body.password) {
-            return res.status(400).json({ message: "Password is required" });
-        }
-
-        const newHash = SHA256(req.body.password + loginUser.salt).toString(encBase64);
-        if (newHash !== loginUser.hash) {
-            return res.status(401).json({ message: "Email or password incorrect" });
-        }
-
-        res.status(200).json({
-            _id: loginUser._id,
-            token: loginUser.token,
-            account: {
-                username: loginUser.username,
-            },
-        });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+  try {
+    const loginUser = await User.findOne({ email: req.body.email });
+    if (!loginUser) {
+      return res.status(401).json({ message: "Email or password incorrect" });
     }
+    if (!req.body.password) {
+      return res.status(400).json({ message: "Password is required" });
+    }
+
+    const newHash = SHA256(req.body.password + loginUser.salt).toString(
+      encBase64
+    );
+    if (newHash !== loginUser.hash) {
+      return res.status(401).json({ message: "Email or password incorrect" });
+    }
+
+    res.status(200).json({
+      _id: loginUser._id,
+      token: loginUser.token,
+      account: {
+        username: loginUser.username,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+/*---FAVORIS-----*/
+
+app.post("/user/favorites/add", async (req, res) => {
+  const { userId, characterId } = req.body;
+
+  try {
+    // Trouver l'utilisateur par son ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+
+    // Vérifier si le personnage est déjà dans les favoris de l'utilisateur
+    if (!user.favorites.includes(characterId)) {
+      // Ajouter le personnage à la liste des favoris de l'utilisateur
+      user.favorites.push(characterId);
+
+      // Enregistrer les modifications
+      await user.save();
+
+      return res.status(200).json({ message: "Favori ajouté avec succès" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Ce personnage est déjà en favori" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Erreur lors de l'ajout du favori" });
+  }
 });
 
+app.post("/user/favorites/remove", async (req, res) => {
+  const { userId, characterId } = req.body;
+
+  try {
+    // Trouver l'utilisateur par son ID
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+
+    // Vérifier si le personnage est dans les favoris de l'utilisateur
+    const index = user.favorites.indexOf(characterId);
+    if (index !== -1) {
+      // Supprimer le personnage de la liste des favoris de l'utilisateur
+      user.favorites.splice(index, 1);
+
+      // Enregistrer les modifications
+      await user.save();
+
+      return res.status(200).json({ message: "Favori supprimé avec succès" });
+    } else {
+      return res
+        .status(400)
+        .json({ message: "Ce personnage n'est pas un favori" });
+    }
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la suppression du favori" });
+  }
+});
 /*-----CHECKING ROUTE------*/
 
 app.get(`/`, (req, res) => {
-    res.status(200).json({ message: `Welcome to Marvel server` });
+  res.status(200).json({ message: `Welcome to Marvel server` });
 });
 
 app.all("*", (req, res) => {
-    res.status(404).json("Not found");
+  res.status(404).json("Not found");
 });
 
 app.listen(process.env.PORT, () => {
-    console.log("Server started, GO......");
+  console.log("Server started, GO......");
 });
--- models/user.js
+++ models/user.js
@@ -1,19 +1,20 @@
 const mongoose = require("mongoose");
 
 const User = mongoose.model("User", {
-    email: {
-        unique: true,
-        type: String,
+  email: {
+    unique: true,
+    type: String,
+  },
+  account: {
+    username: {
+      required: true,
+      type: String,
     },
-    account: {
-        username: {
-            required: true,
-            type: String,
-        },
-    },
-    token: String,
-    hash: String,
-    salt: String,
+  },
+  token: String,
+  hash: String,
+  salt: String,
+  favorites: Array,
 });
 
 module.exports = User;
